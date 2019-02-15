7b8dac9f336eb22b7f6bf961f486de430fbecebc
Ashley Renee Smith <ashleyreneesmith@zipcoders-MacBook-Pro-2.local>
2019-02-15 18:05:57 -0500

    first commit for my first time

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,122 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int zipcode;
+    private String state;
+    private Double height;
+    private int weight;
+    private boolean gender;
+
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+
+        this.age = age;
+
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
     }
 
+
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+//
+/*
+    public Person() {
+        this.state= "";
+        this.zipcode= Integer.MAX_VALUE;
+    }
+
+    public Person(int zipcode) {
+
+        this.zipcode = zipcode;
+
+    }
+
+    public Person(String state) {
+        this.state = state;
+    }
+
+    public Person(String state, int zipcode) {
+        this.state  = state;
+        this.zipcode = zipcode;
+
+    }*/
+
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getState() {
+
+        return this.state;
+    }
+
+    public Integer getZipCode() {
+        return this.zipcode;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public Double getHeight() {
+        return this.height;
+    }
+
+
+    public void setState(Double expected) {
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    public Boolean getGender() {
+        return this.gender;
+    }
+
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
 }
+
+
+
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.person;
 
 import org.junit.Assert;
 import org.junit.Test;
+import sun.jvm.hotspot.debugger.Address;
 
 /**
  * Created by leon on 2/12/18.
@@ -95,4 +96,169 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSet() {
+        // Given
+        Person person = new Person();
+        Integer expected = 5;
+
+        // When
+        person.setAge(expected);
+
+        // Then
+        Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+
+
+    /*@Test
+    public void testSecondConstructor() {
+        // Given
+        String expectedState = "";
+        Integer expectedZipCode = Integer.MAX_VALUE;
+
+        // When
+        Person address = new Person();
+
+        // Then
+        String actualState = address.getState();
+        Integer actualZipCode = address.getZipCode();
+
+        Assert.assertEquals(expectedState, actualState);
+        Assert.assertEquals(expectedZipCode, actualZipCode);*/
+    }
+
+    /*@Test
+    public void testConstructorWithState() {
+        // Given
+        String expected = "Delaware";
+
+        // When
+        Person address = new Person(expected);
+
+        // Then
+        String actual = address.getState();
+        Assert.assertEquals(expected, actual);*/
+
+
+    /*@Test
+    public void testConstructorWithZipCode() {
+        // Given
+        String expected = "19701";
+
+        // When
+        Person address = new Person(expected);
+
+        // Then
+        Integer actual = address.getZipCode();
+        Assert.assertEquals(expected, actual);*/
+
+
+
+    /*@Test
+    public void testConstructorWithStateAndZipCode() {
+        // Given
+        Integer expectedZipCode = 19701;
+        String expectedState = "Delaware";
+
+        // When
+        Person address = new Person(expectedState, expectedZipCode);
+
+        // Then
+        Integer actualZipCode = address.getZipCode();
+        String actualState = address.getName();
+
+        Assert.assertEquals(expectedZipCode, actualZipCode);
+        Assert.assertEquals(expectedState, actualState);*/
+
+
+    /*@Test
+    public void testSetState() {
+        // Given
+        Person address = new Person();
+        String expected = "Delaware";
+
+        // When
+        address.setState(expected);
+        String actual = address.getState();
+
+        // Then
+        Assert.assertEquals(expected, actual); */
+
+
+    @Test
+    public void testSetZipCode() {
+        // Given
+        Person zipcode = new Person();
+        Integer expected = 19701;
+
+        // When
+        zipcode.setZipCode(expected);
+
+        // Then
+        Integer actual = zipcode.getZipCode();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetAddress() {
+        // Given
+        Person address = new Person();
+        Integer expected = 19701;
+
+        // When
+        address.setZipCode(expected);
+
+        // Then
+        Integer actual = address.getZipCode();
+        Assert.assertEquals(expected, actual);
+
+    }
+    //
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person height = new Person();
+        Double expected = 5.5;
+
+        // When
+        height.setHeight(expected);
+        Double actual = height.getHeight();
+
+        // Then
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWeight() {
+        // Given
+        Person weight = new Person();
+        Integer expected = 137;
+
+        // When
+        weight.setZipCode(expected);
+
+        // Then
+        Integer actual = weight.getZipCode();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetGender() {
+        // Given
+        Person gender = new Person();
+        Boolean expected = true;
+
+        // When
+        gender.setGender(expected);
+
+        // Then
+        Boolean actual = gender.getGender();
+        Assert.assertEquals(expected, actual);
+
+    }
+
 }
+
+
+
